bf0267e41880d55eb2ee81df9e328af61f0dbc06
Jim Thompson <jimthompson5802@gmail.com>
2021-11-26 10:28:18 -0500

    clean-out deprecated comments

--- dask/k8s_manifests/launch_dask.sh
+++ dask/k8s_manifests/launch_dask.sh
@@ -1,22 +1,4 @@
-# KUBEFLOW-USER="<User id>"                        #Put your User id in quotes
-# export server_url="<docker-registry-url>"  #private docker registry url
-# export repository="ssdsp-user-${KUBEFLOW-USER}"     #your repository
-# export image_name="dask"                         #name of the image you want to give it
-# export version_number="v1"                       #version of the image
-# export notebook_claimName="workspace-dask-demo"  #name of your workspace volume
-# export docker-registry-secret="docker-registry-pull-secret" #secret for pulling from private docker-registry
-#
-# export worker_replicas="3"          #number of worker pods
-#
-# export scheduler_cpu_limit="24"     #use these as the default values. 24,24, 48G,48G
-# export scheduler_cpu_request="24"
-# export scheduler_memory_limit="48G"
-# export scheduler_memory_request="48G"
-#
-# export worker_cpu_limit="12"         #use these as the default values.  12,12, 24G,24G
-# export worker_cpu_request="12"
-# export worker_memory_limit="24G"
-# export worker_memory_request="24G"
+#!/bin/bash
 
 docker_repository='registry.hub.docker.com'
 dask_image='dsimages/dask_image'
